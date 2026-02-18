538b16b3664eb92a6df7a9f5e4e112c94c74cb8e
jboschlagos <jboschlagos@gmail.com>
2026-02-18 19:46:26 -0300

    modificaciones varias entorno a la eficiencia del servidor y los vinculos con los productos.

--- app.js
+++ app.js
@@ -36,16 +36,16 @@ const tienda = {
     nombre: "MiniTienda",
     mensajeBienvenida: "Descubre las mejores prendas con estilo único",
     productos: [
-        { nombre: "Camiseta Básica", precio: 15000, disponible: true, imagen: "https://images.unsplash.com/photo-1523381294911-8d3cead13475?w=600" },
-        { nombre: "Pantalón Jeans", precio: 30000, disponible: false, imagen: "https://images.unsplash.com/photo-1583005008627-cf9c4e1a9d6d?w=600" },
-        { nombre: "Zapatos Deportivos", precio: 50000, disponible: true, imagen: "https://images.unsplash.com/photo-1528701800489-20be8c01c1a3?w=600" },
-        { nombre: "Chaqueta de Cuero", precio: 80000, disponible: true, imagen: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=600" },
-        { nombre: "Gorra Clásica", precio: 12000, disponible: true, imagen: "https://images.unsplash.com/photo-1526170375885-bf2f5f0f3e3a?w=600" },
-        { nombre: "Bolso de Mano", precio: 45000, disponible: false, imagen: "https://images.unsplash.com/photo-1526170375885-43f5d6d4f00f?w=600" },
-        { nombre: "Reloj Digital", precio: 60000, disponible: true, imagen: "https://images.unsplash.com/photo-1526170375885-6c60d6f0f47f?w=600" },
-        { nombre: "Bufanda de Lana", precio: 18000, disponible: true, imagen: "https://images.unsplash.com/photo-1526170375885-6b73d6d0f0aa?w=600" },
-        { nombre: "Sudadera Hoodie", precio: 35000, disponible: false, imagen: "https://images.unsplash.com/photo-1526170375885-9f25d6f0f077?w=600" },
-        { nombre: "Gafas de Sol", precio: 25000, disponible: true, imagen: "https://images.unsplash.com/photo-1526170375885-fc40d6f0f0cc?w=600" }
+        { nombre: "Camiseta Básica", precio: 15000, disponible: true, imagen: "/img/camiseta_basica.png" },
+        { nombre: "Pantalón Jeans", precio: 30000, disponible: false, imagen: "/img/pantalon_jeans.png" },
+        { nombre: "Zapatos Deportivos", precio: 50000, disponible: true, imagen: "/img/zapatos_deportivos.png" },
+        { nombre: "Chaqueta de Cuero", precio: 80000, disponible: true, imagen: "/img/chaqueta_cuero.png" },
+        { nombre: "Gorra Clásica", precio: 12000, disponible: true, imagen: "/img/gorra_clasica.png" },
+        { nombre: "Bolso de Mano", precio: 45000, disponible: false, imagen: "/img/toiletri_bag.png" },
+        { nombre: "Reloj Digital", precio: 60000, disponible: true, imagen: "/img/reloj_digital.png" },
+        { nombre: "Bufanda de Lana", precio: 18000, disponible: true, imagen: "/img/bufanda.png" },
+        { nombre: "Sudadera Hoodie", precio: 35000, disponible: false, imagen: "/img/sudadera_hoodie.png" },
+        { nombre: "Gafas de Sol", precio: 25000, disponible: true, imagen: "/img/lentes_de_sol.png" }
     ]
 };
 
